e59311098be60e5e9bb36912c1a74f94e30d3dfa
Nolan Trapp <officialstackemup@gmail.com>
2020-05-21 18:06:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -309,7 +309,7 @@ bot.on('message', msg=>{
 
        // msg.channels.get('log-stuff').send(" ATTN!!!!" + member.user.tag + " has said something Bad!!")
         
-       Client.channels.get("log-stuff").then( message.channel.send("ATTN!! " + message.author.toString() + ", has said something BAD!!"))
+       Client.channels.find("log-stuff").then( msg.channel.send("ATTN!! " + message.author.toString() + ", has said something BAD!!"))
        
 
 
