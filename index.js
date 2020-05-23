98ad480cb1554990b7e95a1615fcaff9c93c83db
Nolan Trapp <officialstackemup@gmail.com>
2020-05-22 21:12:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ bot.on('message', msg=>{
             .addField('Yoda says', 'Only Just Begun, The meme war has')
             .addField('Did you ever hear the Tragedy of Darth Plagueis the wise?'
             , ' I thought not. Its not a story the Jedi would tell you. Its a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Its ironic he could save others from death, but not himself.')
-
+        .addField("Server Mom!!", "Kara is not the server mom, lol")
 
            msg.reply(Embed)
            console.log(score)
