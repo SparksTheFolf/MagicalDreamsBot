55280f0acd15d003965bb494e0acabe01ec5cefe
Nolan Trapp <officialstackemup@gmail.com>
2020-05-27 22:52:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const ping = require('minecraft-server-util')
 //bot.login(process.env.token);
 //bot.login(token)
 
-bot.on('ready' , (oldMessage, newMessage, content) =>{
+bot.on('ready' , (oldMessage, newMessage, Content) =>{
     console.log('MDBot is Online!');
 
     bot.user.setActivity('for md!help', {type: 'WATCHING'})
     
-     if(oldMessage.content === newMessage.content){
+     if(oldMessage.Content === newMessage.Content){
             return;
      }
 })
