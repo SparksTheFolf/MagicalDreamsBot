a81a7e8d9ca4b5a64d3c5a446e9a277910f77ce0
Nolan Trapp <officialstackemup@gmail.com>
2020-09-18 13:58:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -139,20 +139,11 @@ setInterval(() => {
 
 
 bot.on("guildMemberAdd", member => {
-    member.send("Welcome to our server " + message.author.username + " ,please read the rules in the #rules-and-info channel! ")
+    member.send("Welcome to our server " + message.author.username , message.author.avatarURL + "! ")
     member.send("Here are the rules:")
-    member.send("• No spamming in chats - This includes but is not limited to, loud/obnoxious noises in voice, @mention spam, character spam, image spam, and message")
-    member.send("• Do not make fun of, or bully other park guests. - Keeping a friendly, fun environment is enjoyable for all guests!")
-    member.send("• No advertising other servers or organizations. - Do not link to other Minecraft/Discord servers. - Do not link YouTube or Twitch channels. - Do not link any social media platforms (Twitter, Instagram, Facebook, etc). This excludes the official MagicalDreams platforms and/or any of our partners.")
-    member.send("• Sharing of personal information (like passwords) or doxing is prohibited. ")
-    member.send("• Don't post NSFW/offensive/harmful content.")
-    member.send("• Use the appropriate channels and abide by their descriptions.")
-    member.send("• Please avoid using offensive or vulgar language, we maintain a family-friendly network")
-    member.send("• Do not ping staff members or executives constantly or for no reason")
-    member.send("• Respect all staff member decisions, although if you feel like a staff member is abusing their power - contact a Manager.")
-    member.send("• If a post or rule is in question, contact a Cast Member and they will make the final judgement.")
-    member.send("• Be respectful to one another and do not cause drama.")
-    member.send("• Breaking these rules could result in a kick, ban, or mute. Follow these rules and have fun!!")
+    member.send("• No spamming in chats - This includes but is not limited to, loud/obnoxious noises in voice, @mention spam, character spam, image spam, and message • Do not make fun of, or bully other park guests. - Keeping a friendly, fun environment is enjoyable for all guests! • No advertising other servers or organizations. - Do not link to other Minecraft/Discord servers. - Do not link YouTube or Twitch channels. - Do not link any social media platforms (Twitter, Instagram, Facebook, etc). This excludes the official MagicalDreams platforms and/or any of our partners. • Sharing of personal information (like passwords) or doxing is prohibited. ")
+    member.send("• Don't post NSFW/offensive/harmful content. • Use the appropriate channels and abide by their descriptions. • Please avoid using offensive or vulgar language, we maintain a family-friendly network • Do not ping staff members or executives constantly or for no reason")
+    member.send("• Respect all staff member decisions, although if you feel like a staff member is abusing their power - contact a Manager. • If a post or rule is in question, contact a Cast Member and they will make the final judgement. • Be respectful to one another and do not cause drama. • Breaking these rules could result in a kick, ban, or mute. Follow these rules and have fun!!")
         .catch(console.error);
 });
 
