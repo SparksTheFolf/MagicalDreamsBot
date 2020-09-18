dcc4392976f2e2135d554ffe128d8bd6a59aa104
Nolan Trapp <officialstackemup@gmail.com>
2020-09-18 12:31:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use_strict'
 
-const {Client, RichEmbed, message} = require('discord.js')
+const {Client, RichEmbed, message, member, guildMemberAdd} = require('discord.js')
 const E = require('events');
 const request = require('request');
 const cheerio = require('cheerio');
