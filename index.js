00c9487543eb6f35a477cbd25cb34c40e7872b7e
Nolan Trapp <officialstackemup@gmail.com>
2020-09-19 11:32:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -174,34 +174,40 @@ bot.on('message', msg=>{
     }
 
 
-    switch(args[0]){
+    switch (args[0]) {
         case 'mute':
-
-        let person = msg.guild.member(msg.mentions.users.first() || msg.guild.members.get(args[1]))
-        if(!person) return msg.reply("Could't find that member");
-
-        let muterole = msg.guild.roles.find(role => role.name === "Muted")
-
-        if(!muterole) return msg.reply("Culden't find the mute role");
-
-        let time = args[2];
-
-        if(!time){
-            return msg.reply("You didnt specify a time!");
-        }
-
-        person.addRole(muterole.id);
-
-        msg.channel.send(`@${person.user.tag} has been muted for ${ms(ms(time))}`);
-
-        setTimeout(function(){
-
-            person.removeRole(muterole.id);
-
-            msg.channel.send(`@${person.user.tag} has been unmuted`);
-
-        }, ms(time))
-
+            var person  = msg.guild.member(msg.mentions.users.first() || msg.guild.members.get(args[1]));
+            if(!person) return  msg.reply("I CANT FIND THE USER " + person)
+ 
+            let mainrole = msg.guild.roles.find(role => role.name === "{Verified}");
+            let role = msg.guild.roles.find(role => role.name === "mute");
+           
+ 
+            if(!role) return msg.reply("Couldn't find the mute role.")
+ 
+ 
+            let time = args[2];
+            if(!time){
+                return msg.reply("You didnt specify a time!");
+            }
+ 
+            person.removeRole(mainrole.id)
+            person.addRole(role.id);
+ 
+ 
+            msg.channel.send(`@${person.user.tag} has now been muted for ${ms(ms(time))}`)
+ 
+            setTimeout(function(){
+                
+                person.addRole(mainrole.id)
+                person.removeRole(role.id);
+                console.log(role.id)
+                msg.channel.send(`@${person.user.tag} has been unmuted.`)
+            }, ms(time));
+ 
+ 
+    
+        break;
     }
 
 /*
